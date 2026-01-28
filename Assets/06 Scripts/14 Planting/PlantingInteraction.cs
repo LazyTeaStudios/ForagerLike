b7fa504ac05a2641bf17d54c3a150a2514fd1766
Lawrence <b8024844@hallam.shu.ac.uk>
2026-01-28 13:39:15 +0000

    UI Guide

--- Assets/06 Scripts/14 Planting/PlantingInteraction.cs
+++ Assets/06 Scripts/14 Planting/PlantingInteraction.cs
@@ -3,40 +3,79 @@ using UnityEngine;
 public class PlantingInteraction : MonoBehaviour
 {
     [Header("References")]
-    private Camera playerCamera;
-    private float interactionRange = 5f;
+    [SerializeField] GameObject canPlantIndicator;
 
-    [SerializeField] private LayerMask seedBedMask = -1;
+    [Header("Settings")]
+    [SerializeField] float interactionRange = 5f;
+    [SerializeField] LayerMask seedBedMask = -1;
+
+    Camera playerCamera;
+    bool indicatorState;
 
     void Awake()
     {
-        if (playerCamera == null)
-            playerCamera = Camera.main;
+        playerCamera = Camera.main;
+
+        if (canPlantIndicator != null)
+            canPlantIndicator.SetActive(false);
+
+        indicatorState = false;
     }
 
     void Update()
     {
         if (InputHandler.IsMapActive(ActionMap.UI))
+        {
+            SetIndicator(false);
             return;
+        }
+
+        UpdatePlantIndicator();
 
         if (InputHandler.Pressed(GameAction.GameplayMouseLeftClick))
-        {
             TryPlantSeed();
+    }
+
+    void UpdatePlantIndicator()
+    {
+        InventorySlot selectedSlot = InventoryManager.Instance.GetSelectedHotbarSlot();
+        bool hasSeedSelected = selectedSlot != null && selectedSlot.item != null && selectedSlot.item.itemType == ItemType.Seed;
+
+        if (!hasSeedSelected)
+        {
+            SetIndicator(false);
+            return;
         }
+
+        if (playerCamera == null)
+            playerCamera = Camera.main;
+
+        if (playerCamera == null)
+        {
+            SetIndicator(false);
+            return;
+        }
+
+        Ray ray = playerCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+        if (Physics.Raycast(ray, out RaycastHit hit, interactionRange, seedBedMask))
+        {
+            SeedBed seedBed = hit.collider.GetComponentInParent<SeedBed>();
+            if (seedBed != null && !seedBed.IsOccupied())
+            {
+                SetIndicator(true);
+                return;
+            }
+        }
+
+        SetIndicator(false);
     }
 
     void TryPlantSeed()
     {
         InventorySlot selectedSlot = InventoryManager.Instance.GetSelectedHotbarSlot();
-        if (selectedSlot == null)
-            return;
-
-
-        if (selectedSlot.item == null)
-            return;
-
-        if (selectedSlot.item.itemType != ItemType.Seed)
-            return;
+        if (selectedSlot == null) return;
+        if (selectedSlot.item == null) return;
+        if (selectedSlot.item.itemType != ItemType.Seed) return;
 
         if (playerCamera == null)
             playerCamera = Camera.main;
@@ -57,13 +96,19 @@ public class PlantingInteraction : MonoBehaviour
             if (seedBed.PlantSeed(selectedSlot.item))
             {
                 InventoryManager.Instance.RemoveItem(selectedSlot.item, 1);
-                //Debug.Log($"Planted {selectedSlot.item.itemName}");
-            }
-            else
-            {
-                Debug.Log("Seed bed is already occupied!");
+                SetIndicator(false);
             }
         }
     }
 
+    void SetIndicator(bool on)
+    {
+        if (indicatorState == on)
+            return;
+
+        indicatorState = on;
+
+        if (canPlantIndicator != null)
+            canPlantIndicator.SetActive(on);
+    }
 }
